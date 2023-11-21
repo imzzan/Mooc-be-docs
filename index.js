a00670cfd2a65c523bd8158e1aace1533cc8f464
TugasKu23 <kutugas23@gmail.com>
2023-11-21 17:49:13 +0700

    firts commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json')
+
+const app = express();
+const PORT = 3004;
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('Kalo mau melihat dokumentasi bisa kasih /api-docs ya di endpointnya terima kasih :v')
+})
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+})
